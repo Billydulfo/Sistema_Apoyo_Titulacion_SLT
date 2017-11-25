3389f4ff34455a982d0c2695e0b1cbd9f213f72a
U-Lee <32946965+U-Lee@users.noreply.github.com>
2017-11-25 00:39:06 -0600

    Guarda el objeto asesor a la base de datos.

---
+++ Sistema_Apoyo_Titulacion_SLT/src/controlador/GuardarBDAsesor.java
@@ -0,0 +1,23 @@
+package controlador;
+
+//Ajustar los datos de acuerdo a la base de datos local
+public class GuardarBDAsesor{
+
+public void guardarBD(Asesor asesor) {
+        String cadena = "jdbc:postgresql://localhost:5432/prueba";     //ajustar
+        String user = "postgres";     //ajustar
+        String pass = "123";     //ajustar
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conex = (Connection) DriverManager.getConnection(cadena, user, pass);    //Conectar a la base de datos
+            java.sql.Statement st = conex.createStatement();    //No agarra sin añadir la linea java.sql
+            String sql = "INSERT INTO asesores (nombre, apellido_paterno, apellido_materno, telefono, cubiculo, correo_electronico) VALUES ('" + asesor.getnombre() + "', '" + asesor.getapellidoPaterno() + "', '" + asesor.getapellidoMaterno() + "', '" + asesor.gettelefono() + "', '" + asesor.getcubiculo() + "', '" + asesor.getcorreoElectronico() + "')";
+            st.executeUpdate(sql);  //Actualiza la base de datos
+            st.close();
+            conex.close();
+        } catch (Exception e) {
+            System.out.println("No se pudo agregar el asesor a la base de datos.");
+        }
+    }
+}
