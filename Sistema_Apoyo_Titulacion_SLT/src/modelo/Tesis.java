9d7344729d226e91f26d73cb69b81d521651fdc5
Oswaldo Hernández <33918023+oswaldohernandez11@users.noreply.github.com>
2017-11-24 21:32:30 -0600

    Actualización de clase

--- Sistema_Apoyo_Titulacion_SLT/src/modelo/Tesis.java
+++ Sistema_Apoyo_Titulacion_SLT/src/modelo/Tesis.java
@@ -1,22 +1,16 @@
-
 package modelo;
-
 public class Tesis {
     private String Tema;
     private Asesor director;
-    
     public String getTema() {
         return Tema;
     }
-
     public void setTema(String Tema) {
         this.Tema = Tema;
     }
-
     public Asesor getDirector() {
         return director;
     }
-
     public void setDirector(Asesor director) {
         this.director = director;
     }
