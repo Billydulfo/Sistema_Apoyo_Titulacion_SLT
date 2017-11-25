385a24635d1f951cc135cecd5df391d847d2c584
Oswaldo Hernández <33918023+oswaldohernandez11@users.noreply.github.com>
2017-11-24 21:25:18 -0600

    Clase con constructores

--- Sistema_Apoyo_Titulacion_SLT/src/modelo/Asesor.java
+++ Sistema_Apoyo_Titulacion_SLT/src/modelo/Asesor.java
@@ -4,6 +4,14 @@ public class Asesor extends Persona{
     private String cubiculo;
     private Horario horarioAsesorias;
 
+    public Asesor(String cubiculo, Horario horarioAsesorias, String nombre,
+            String apellidoPaterno, String apellidoMaterno, 
+            String correoElectronico, int telefono) {
+        super(nombre, apellidoPaterno, apellidoMaterno, correoElectronico, telefono);
+        this.cubiculo = cubiculo;
+        this.horarioAsesorias = horarioAsesorias;
+    }
+    
     public Horario getHorarioAsesorias() {
         return horarioAsesorias;
     }
