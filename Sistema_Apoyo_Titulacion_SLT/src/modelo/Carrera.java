b0a761693381183231c7de2918bb4806c9b544ed
Oswaldo Hernández <33918023+oswaldohernandez11@users.noreply.github.com>
2017-11-24 21:26:50 -0600

    Clase actualizada

--- Sistema_Apoyo_Titulacion_SLT/src/modelo/Carrera.java
+++ Sistema_Apoyo_Titulacion_SLT/src/modelo/Carrera.java
@@ -1,20 +1,23 @@
-
 package modelo;
 
 public class Carrera {
     private String nombre;
-
+    private String colegio;
     public String getNombre() {
         return nombre;
     }
-
+    public String getColegio() {
+        return colegio;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    public void setColegio(String colegio) {
+        this.colegio = colegio;
+    }
     @Override
     public String toString() {
-        return "Carrera{"+"nombre="+nombre+'}';
+        return "Carrera{"+"nombre="+nombre+"colegio="+colegio+'}';
     }
     
     
