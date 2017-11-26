8b110e05dc283eb2fa52fb46a31d06419a6e1ed0
Billydulfo <33133495+Billydulfo@users.noreply.github.com>
2017-11-25 22:23:56 -0600

    la actualización utiliza otra pantalla para capturar candidato y aun no se configura el administrador de base de datos para accesar a bd17

---
+++ Sistema_Apoyo_Titulacion_SLT/src/controlador/ControladorGuardaCandidato.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import modelo.AdminBD;
+import modelo.Candidato;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import modelo.Grupo;
+import modelo.Tesis;
+import modelo.Trabajo;
+import vista.JPanelCreateCandidato;
+
+/**
+ *
+ * @author Jhon Nash
+ */
+public class ControladorGuardaCandidato implements ActionListener{
+    private Grupo grupo;
+    private Candidato candidato;
+    private Tesis tesis;
+    private Trabajo trabajo;
+    private AdminBD adminBD;
+    private JPanelCreateCandidato jPanelCreateCandidato;
+    public void actionPerformed(ActionEvent e){
+        candidato = new Candidato();
+        tesis = new Tesis();
+        jPanelCreateCandidato = new JPanelCreateCandidato();
+        adminBD = new AdminBD();
+        candidato.setNombre(jPanelCreateCandidato.jtxtNombre.getText());
+        candidato.setApellidoPaterno(jPanelCreateCandidato.jtxtApellidoPaterno.getText());
+        candidato.setApellidoMaterno(jPanelCreateCandidato.jtxtApellidoMaterno.getText());
+        candidato.setCorreoElectronico(jPanelCreateCandidato.jtxtCorrElectronico.getText());
+        candidato.setTelefono(Integer.parseInt(jPanelCreateCandidato.jtxtTelefono.getText()));
+        candidato.setMatricula(jPanelCreateCandidato.jtxtMatricula.getText());
+        candidato.setGeneracion(jPanelCreateCandidato.jtxtGeneracion.getText());
+        candidato.setCarrera(jPanelCreateCandidato.jComBoxCarrera.getSelectedItem().toString());
+        /*acciones para tesis*/
+        tesis.setTema(jPanelCreateCandidato.jtxtNomTesis.getText());
+        tesis.setDirector(jPanelCreateCandidato.jtxtDircTesis.getText());
+        candidato.setTesis(tesis);
+        /*acciones para trabajo*/
+        trabajo.setNombreEntidad(jPanelCreateCandidato.jtxtEntidad.getText());
+        candidato.setTrabajo(trabajo);
+        /*falta incorporar obtencion de carta compromiso
+        *obtencion de carta de exposicion de motivos
+        *obtencion de foto del candidato, tambien subir foto
+        *captura de horarios y subir carreras desde postgresql*/
+        
+        /*agregamos a la base de datos*/
+        grupo.setCandidato(candidato);
+        adminBD.insertarCandidato(candidato);
+    }
+    
+public void asignaGrupo(Grupo grupo){
+    this.grupo = grupo;
+}
+    
+}
